9ed7cc87d839b80b5553bbe142316599507c9a67
YanaSota <b2212490@photon.chitose.ac.jp>
2022-11-01 21:18:57 +0900

    課題2

---
+++ src/lecture01/kadai1_2.java
@@ -0,0 +1,14 @@
+package lecture01;
+
+public class kadai1_2 {
+
+        public static void main(String[] args) {
+            print(" Sota Yanagida");
+        }
+
+        static void print(String str){
+            int a = 2212490;
+            String StudentNumber = "B"+a;
+            System.out.println(StudentNumber+str);
+        }
+}
