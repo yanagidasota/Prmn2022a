e484fa4c0c647b1f88bb85d90c616d608b6b1d51
YanaSota <b2212490@photon.chitose.ac.jp>
2022-11-01 21:04:29 +0900

    課題１

---
+++ src/lecture01/Main.java
@@ -0,0 +1,11 @@
+package lecture01;
+
+public class Main {
+    public static void main(String[] args) {
+        print("B2212490 Sota Yanagida");
+    }
+
+    static void print(String str){
+        System.out.println(str);
+    }
+}
