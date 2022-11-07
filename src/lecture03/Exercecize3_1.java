688580699fa49b8a13f49e49ef32a32b3b034060
YanaSota <b2212490@photon.chitose.ac.jp>
2022-11-07 23:29:16 +0900

    課題3_1

--- src/lecture03/Exercecize3_1.java
+++ src/lecture03/Exercecize3_1.java
@@ -1,4 +1,12 @@
 package lecture03;
 
+import java.util.Scanner;
 public class Exercecize3_1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("任意の文字列を入力してください:");
+        String str = inp.nextLine();
+        System.out.println("”"+str+"”と入力されました");
+    }
 }
