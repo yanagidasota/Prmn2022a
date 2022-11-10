f3a151362303a9c31c6a88071b25e195c2165a85
YanaSota <b2212490@photon.chitose.ac.jp>
2022-11-10 22:45:28 +0900

    課題5_3

--- src/lecture05/Exercise5_3.java
+++ src/lecture05/Exercise5_3.java
@@ -2,6 +2,7 @@ package lecture05;
 
 public class Exercise5_3 {
     public static void main(String[] args) {
-        
+        Insect butterfly = new Butterfly();
+        butterfly.move();
     }
 }
