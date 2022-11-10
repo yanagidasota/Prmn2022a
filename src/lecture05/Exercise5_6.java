ef09eb58d6d141657a44bf27521f753fb9079f7d
YanaSota <b2212490@photon.chitose.ac.jp>
2022-11-10 22:54:51 +0900

    課題5_5

---
+++ src/lecture05/Exercise5_6.java
@@ -0,0 +1,8 @@
+package lecture05;
+
+public class Exercise5_6 {
+    public static void main(String[] args) {
+        Insect s_butterfly = new SwallowtailButterfly();
+        s_butterfly.move();
+    }
+}
---
+++ src/lecture05/SwallowtailButterfly.java
@@ -0,0 +1,7 @@
+package lecture05;
+
+public class SwallowtailButterfly extends Butterfly{
+    void move(){
+        System.out.println("綺麗に跳んだよ");
+    }
+}
